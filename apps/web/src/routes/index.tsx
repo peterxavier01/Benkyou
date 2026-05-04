3f960173a112e4ee9c91138756d80f8f6b9f57ba
peterxavier01 <pletepeers@gmail.com>
2026-05-04 23:09:04 +0100

    style: standardize formatting and improve readability across BetterAuthHeader, Tanstack Query components, and Home route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,14 +1,13 @@
+import { Button } from "#/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: Home });
 
 function Home() {
-	return (
-		<div className="p-8">
-			<h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
-			<p className="mt-4 text-lg">
-				Edit <code>src/routes/index.tsx</code> to get started.
-			</p>
-		</div>
-	);
+  return (
+    <div className="p-8 space-y-4">
+      <h1 className="text-5xl font-bold">Benkyou</h1>
+      <Button>Let's Go</Button>
+    </div>
+  );
 }
